237053679029de7befec5732e9fe296547f05812
rafa-carmo <rafaelcarmo_15@hotmail.com>
2020-11-22 15:38:41 -0300

    set operation

--- App.js
+++ App.js
@@ -36,16 +36,36 @@ export default class App extends React.Component {
       const newValue = parseFloat(displayValue)
       const values = [...this.state.values]
       values[this.state.current] = newValue
+      this.setState({values})
     }
 
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: '0' })
+    this.setState({ ...initialState })
   }
 
-  setOperation = () => {
+  setOperation = operation => {
+    if (this.state.current === 0){
+      this.setState({operation, current: 1, clearDisplay: true})
+    }else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch(e) {
+        values[0] = this.state.values[0]
+      }
 
+      values[1] = 0
+      this.setState({ 
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current : equals ? 0 : 1,
+        clearDisplay : !equals,
+        values, 
+      })
+    }
   }
   render(){
   return (
